8d6b17a0e684bdf6af1442f1de17ee3dac496a7f
Wiktoria <viktoria.zavadzka@gmail.com>
2019-11-10 00:19:43 +0100

    react jsx translation added

---
+++ src/index.jsx
@@ -0,0 +1,65 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//import './scss/index.scss';
+// Components
+// import Link from './components/Link';
+
+// const App = (
+// 	<div className="app" data-js="app-main">
+// 		<header className="header" data-js="header-container">
+// 			<img alt="Logo" src="../assets/react.png"/>
+// 			<span className="sym-add">+</span>
+// 			<img alt="Logo" src="../assets/electron.png"/>
+// 		</header>
+// 		<section>
+// 			<h2>A Simplified Electron with React Boilerplate Project</h2>
+// 			<p>This bare bones application provides a basis to develop and build your Electron Application with ReactJS.</p>
+// 		</section>
+// 		<footer className="footer" data-js="footer-container">
+// 			<div className="link-container" data-js="link-container">
+// 				<Link className="link" data-js="link-medium" isExternal={true} href="https://medium.com/@mitchpierias/the-dream-team-react-with-electron-c808ecb5b55e">Medium</Link>
+// 				<Link className="link" data-js="link-github" isExternal={true} href="https://github.com/MitchPierias/React-Electron-Boilerplate">Github</Link>
+// 			</div>
+// 			<span className="caption" data-js="footer-caption">
+// 				<small>Remember to spread the love!</small>
+// 				<small>Give stars on Github and claps on Medium</small>
+// 			</span>
+// 		</footer>
+// 	</div>
+// )
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import NavBar from "./nav";
+// import {Link} from "react-router-dom";
+import {
+	HashRouter,
+	Route,
+	Link,
+	Switch,
+	NavLink,
+} from 'react-router-dom';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	render() {
+		return (
+<>
+	<p>jjjj</p>
+	</>
+		)
+	}
+}
+
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
  * @note Some props are injected and some are configuration (rendering) settings
  */
 const htmlPlugin = new HtmlWebPackPlugin({
-	title: "Invoice App",
-	template: "./src/index.html",
-	filename: "./index.html"
+    title: "Invoice App",
+    template: "./src/index.html",
+    filename: "./index.html"
 });
 
 const hotModulePlugin = new webpack.HotModuleReplacementPlugin();
@@ -20,57 +20,77 @@ const miniCssPlugin = new MiniCssExtractPlugin({filename: "[name].css", chunkFil
  * Webpack Configuration
  */
 module.exports = {
-	entry: ['webpack/hot/dev-server','./src/index.js'],
-	target: 'electron-renderer',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		publicPath: './',
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: [
-					/node_modules/,
-					/.json?/
-				],
-				use: {
-					loader: 'babel-loader',
-					query: {
-						presets: ["env","react"]
-					}
-				}
-			}, {
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader']
-			}, {
-				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-			}
-		]
-	},
-	plugins:[
-		htmlPlugin,
-		hotModulePlugin,
+    entry: ['webpack/hot/dev-server', './src/index.jsx'],
+    target: 'electron-renderer',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: './',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    /node_modules/,
+                    /.json?/
+                ],
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ["env", "react"]
+                    }
+                }
+            }, {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env', '@babel/preset-react'
+                        ],
+                        "plugins": [
+                            [
+                                "@babel/plugin-proposal-class-properties", {
+                                "loose": true
+                            }
+                            ]
+                        ]
+                    }
+                }
+            },
+
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }, {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        htmlPlugin,
+        hotModulePlugin,
         miniCssPlugin
-	],
-	resolve: {
-		extensions: ['.js','.jsx']
-	},
-	devServer: {
-		publicPath:'http://localhost:9000',
-		contentBase: path.join(__dirname, 'assets'),
-		open: false,
-		lazy: false,
-		compress: true,
-		historyApiFallback: true,
-		port: 9000,
-		proxy: {
-			'/assets/*': {
-				target: 'http://localhost:9000/',
-				pathRewrite: {'^/assets': ''},
-			}
-		}
-	}
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        publicPath: 'http://localhost:9000',
+        contentBase: path.join(__dirname, 'assets'),
+        open: false,
+        lazy: false,
+        compress: true,
+        historyApiFallback: true,
+        port: 9000,
+        proxy: {
+            '/assets/*': {
+                target: 'http://localhost:9000/',
+                pathRewrite: {'^/assets': ''},
+            }
+        }
+    }
 }
